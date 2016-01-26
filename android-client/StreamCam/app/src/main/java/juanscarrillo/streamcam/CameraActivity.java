e0c3d384b821975b5cddf4eb652f46bdacefe2ca
Juan S Carrillo <juanscarrillo23@gmail.com>
2016-01-25 20:11:31 -0800

    Added logic to access the camera.

--- android-client/StreamCam/app/src/main/java/juanscarrillo/streamcam/CameraActivity.java
+++ android-client/StreamCam/app/src/main/java/juanscarrillo/streamcam/CameraActivity.java
@@ -2,18 +2,25 @@ package juanscarrillo.streamcam;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Camera;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
@@ -21,11 +28,16 @@ import android.support.v4.app.NavUtils;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
+
+    private static final String TAG = CameraActivity.class.getSimpleName();
 
     private CameraManager cameraManager;
-    private String backCameraId;
-    private String frontCameraId;
+    private CameraDevice mCamera;
+    private CameraCaptureSession mSession;
+    private String backCameraId = null;
+    private String frontCameraId = null;
+    private int mCaptureImageFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,67 +48,105 @@ public class CameraActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        Log.d(TAG, "We've started the camera activity");
+        setup_camera();
 
-        initializeCamera();
     }
 
-    private void initializeCamera() {
-        getCameraIds();
-        getBackCamera();
+    // Everything in the following group needs to be implemented
+    // from TextureView.SurfaceTextureListener
+
+    // *--------------------------------------------------------*
+    // |                                                        |
+    // |           TextureView.SurfaceTextureListener           |
+    // *--------------------------------------------------------*
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height){
+        try{
+            setup_camera();
+        }catch(CameraAccessException e){
+            Log.e(TAG, "Error when setting up camera", e);
+        }
     }
 
-    private void getCameraIds() {
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        if (mCamera != null) {
+            mCamera.close();
+            mCamera = null;
+        }
+        mSession = null;
+        return true;
+    }
+
+    // Empty body
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Nothing here
+    }
+
+    // Empty body
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // Nothing here
+    }
+
+    // ==================================================================
+
+    private void setup_camera(){
+        try {
+            getCameraIds();
+            setupCameraFormat();
+            setupCameraCaptureSize();
+        }
+        catch (CameraAccessException e) {
+            Log.e(TAG, "Getting error setting up camera" ,e);
+        }
+    }
+
+    private void getCameraIds() throws CameraAccessException  {
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            String[] camerasOnDevice = cameraManager.getCameraIdList();
-            for(String cameraId : camerasOnDevice) {
-                CameraCharacteristics cameraInfo = cameraManager.getCameraCharacteristics(cameraId);
+        String[] camerasOnDevice = cameraManager.getCameraIdList();
+        for(String cameraId : camerasOnDevice) {
+            CameraCharacteristics cameraInfo = cameraManager.getCameraCharacteristics(cameraId);
 
-                if (cameraInfo.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK) {
-                    backCameraId = cameraId;
-                } else if (cameraInfo.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
-                    frontCameraId = cameraId;
-                }
+            if (cameraInfo.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK) {
+                backCameraId = cameraId;
+            } else if (cameraInfo.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
+                frontCameraId = cameraId;
             }
-        } catch (CameraAccessException e){
-            e.printStackTrace();
+        }
+        if(backCameraId == null || frontCameraId == null) {
+            throw new CameraAccessException(CameraAccessException.CAMERA_ERROR, "Couldn't find suitable camera");
+        }
+
+        Log.d(TAG, "Back Camera is " + backCameraId + " and front camera is " + frontCameraId);
+    }
+
+    private void setupCameraFormat() throws CameraAccessException {
+        CameraCharacteristics cc = cameraManager.getCameraCharacteristics(backCameraId);
+        StreamConfigurationMap streamConfigs = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+        boolean supportsRaw = false, supportsJPEG = false;
+        for(int format : streamConfigs.getOutputFormats()) {
+            if(format == ImageFormat.RAW_SENSOR) {
+                supportsRaw = true;
+            } else if(format == ImageFormat.JPEG) {
+                supportsJPEG = true;
+            }
+        }
+
+        if(supportsRaw) {
+            mCaptureImageFormat = ImageFormat.RAW_SENSOR;
+        } else if(supportsJPEG) {
+            mCaptureImageFormat = ImageFormat.JPEG;
+        } else {
+            throw new CameraAccessException(CameraAccessException.CAMERA_ERROR, "Could not find supported image format");
         }
     }
 
-    private void getBackCamera() {
-        try {
-            cameraManager.openCamera(backCameraId, new CameraDevice.StateCallback() {
-                @Override
-                public void onOpened(CameraDevice camera) {
-                    startPreview(camera);
-                }
-
-                @Override
-                public void onDisconnected(CameraDevice camera) {
-
-                }
-
-                @Override
-                public void onError(CameraDevice camera, int error) {
-
-                }
-
-            },null );
-        } catch (CameraAccessException|SecurityException e) {
-            e.printStackTrace();
-        }
+    private void setupCameraCaptureSize() throws CameraAccessException {
+        
     }
-
-    private void startPreview(CameraDevice camera) {
-        List <Surface> surfacesList = new LinkedList();
-
-        SurfaceView mySurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
-        SurfaceHolder myHolder = mySurfaceView.getHolder();
-        Surface mySurface = myHolder.getSurface();
-        surfacesList.add(mySurface);
-        camera.createCaptureSession(surfacesList, ,);
-    }
-
-
 
 }
