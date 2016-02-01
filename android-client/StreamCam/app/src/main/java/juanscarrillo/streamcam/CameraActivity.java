1a6d09f900587d8f79653f154e9751bd5908c727
Juan S Carrillo <juanscarrillo23@gmail.com>
2016-01-31 18:06:09 -0800

    Removed ugly buttons and changed the camera to fullscreen.

--- android-client/StreamCam/app/src/main/java/juanscarrillo/streamcam/CameraActivity.java
+++ android-client/StreamCam/app/src/main/java/juanscarrillo/streamcam/CameraActivity.java
@@ -129,6 +129,21 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if(hasFocus) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
