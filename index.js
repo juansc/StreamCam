c6871d54afeec53a8e1bc98642bfbb353651513d
Juan S Carrillo <juanscarrillo23@gmail.com>
2016-04-17 16:43:58 -0700

    Started making requests.

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function () {
+
+    var loginRequest = function () {
+    	var requestObject = JSON.stringify({
+    			user: $('#user-name').val(),
+    			password: $('#password').val(),
+    		});
+    	console.log(requestObject);
+    	$.ajax({
+    		type: "POST",
+	        url: "https://stream-cam.herokuapp.com/api/v1/authenticate",
+	        data: requestObject,
+	        contentType: "application/json; charset=utf-8",
+	        dataType: "json",
+	        success: function (data) {
+	            console.log(data.toString());
+	        },
+	        error: function (jqXHR, textStatus, errorThrown) {
+	            console.log(errorThrown);
+	        }
+    	});
+
+
+    };
+
+    $("#login-module").submit(function() {
+    	loginRequest();
+    	return false;
+    });
+
+});
